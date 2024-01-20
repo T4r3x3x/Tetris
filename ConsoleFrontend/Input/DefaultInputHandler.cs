f93ce8c2ea1418c315bec2f64fe64ec9aa68cf54
Mihkail Barsukov <hardbasser322@gmail.com>
2024-01-21 03:12:55 +0700

    Убрал бесполезные async

--- ConsoleFrontend/Input/DefaultInputHandler.cs
+++ ConsoleFrontend/Input/DefaultInputHandler.cs
@@ -2,38 +2,38 @@
 
 namespace ConsoleFrontend.Input
 {
-	public class DefaultInputHandler : IInputHandler
-	{
-		private readonly GameProducer _game;
+    public class DefaultInputHandler : IInputHandler
+    {
+        private readonly GameProducer _game;
 
-		public DefaultInputHandler(GameProducer game)
-		{
-			_game = game;
-		}
+        public DefaultInputHandler(GameProducer game)
+        {
+            _game = game;
+        }
 
-		public async void InputHandle(ConsoleKey key)
-		{
-			switch (key)
-			{
-				case ConsoleKey.A:
-					await _game.MoveFigureLeft();
-					break;
-				case ConsoleKey.D:
-					await _game.MoveFigureRight();
-					break;
-				case ConsoleKey.W:
-					await _game.RotateFigure();
-					break;
-				case ConsoleKey.S:
-					await _game.MoveFigureDown();
-					break;
-				case ConsoleKey.Escape:
-					_game.Pause();
-					break;
-				case ConsoleKey.Enter:
-					_game.Resume();
-					break;
-			}
-		}
-	}
+        public void InputHandle(ConsoleKey key)
+        {
+            switch (key)
+            {
+                case ConsoleKey.A:
+                    _game.MoveFigureLeft();
+                    break;
+                case ConsoleKey.D:
+                    _game.MoveFigureRight();
+                    break;
+                case ConsoleKey.W:
+                    _game.RotateFigure();
+                    break;
+                case ConsoleKey.S:
+                    _game.MoveFigureDown();
+                    break;
+                case ConsoleKey.Escape:
+                    _game.Pause();
+                    break;
+                case ConsoleKey.Enter:
+                    _game.Resume();
+                    break;
+            }
+        }
+    }
 }
--- TetrisEngine/GameProducer.cs
+++ TetrisEngine/GameProducer.cs
@@ -2,314 +2,314 @@
 
 namespace TetrisEngine
 {
-	public class GameProducer
-	{
-		private const int Width = 10, Height = 20;
-		private const double DelayDecrisingPercent = 0.01;
+    public class GameProducer
+    {
+        private const int Width = 10, Height = 20;
+        private const double DelayDecrisingPercent = 0.01;
 
-		private int _countOfErasedRows = 0;
-		private int _delay;
+        private int _countOfErasedRows = 0;
+        private int _delay;
 
-		private readonly Position _startPosition = new(4, 0);
-		private bool isGameOver = false, isPause = false;
+        private readonly Position _startPosition = new(4, 0);
+        private bool isGameOver = false, isPause = false;
 
-		private AbstractFigure _figure;
-		private FigureFactory _figureFactory = new FigureFactory();
+        private AbstractFigure _figure;
+        private FigureFactory _figureFactory = new FigureFactory();
 
-		private Cell[][] _gameField;
+        private Cell[][] _gameField;
 
-		public delegate void GameFieldChangeHandler(Cell[][] cells);
-		public event GameFieldChangeHandler OnGameFieldChanged;
+        public delegate void GameFieldChangeHandler(Cell[][] cells);
+        public event GameFieldChangeHandler OnGameFieldChanged;
 
-		#region Initialization
-		public GameProducer(int startDelay)
-		{
-			_delay = startDelay;
-			InitializeGameField();
-		}
+        #region Initialization
+        public GameProducer(int startDelay)
+        {
+            _delay = startDelay;
+            InitializeGameField();
+        }
 
-		private void InitializeGameField()
-		{
-			_gameField = new Cell[Height][];
-			for (int i = 0; i < Height; i++)
-			{
-				_gameField[i] = new Cell[Width];
-				for (int j = 0; j < _gameField[i].Length; j++)
-					_gameField[i][j] = new Cell(false, Cell.DefaultColor);
-			}
-		}
-		#endregion
+        private void InitializeGameField()
+        {
+            _gameField = new Cell[Height][];
+            for (int i = 0; i < Height; i++)
+            {
+                _gameField[i] = new Cell[Width];
+                for (int j = 0; j < _gameField[i].Length; j++)
+                    _gameField[i][j] = new Cell(false, Cell.DefaultColor);
+            }
+        }
+        #endregion
 
-		#region API
+        #region API
 
-		public int Start()
-		{
-			return ProduceGame();
-		}
+        public int Start()
+        {
+            return ProduceGame();
+        }
 
-		public void Pause()
-		{
-			isPause = true;
-		}
+        public void Pause()
+        {
+            isPause = true;
+        }
 
-		public void Resume()
-		{
-			isPause = false;
-		}
+        public void Resume()
+        {
+            isPause = false;
+        }
 
-		public async Task MoveFigureLeft()
-		{
-			if (isPause)
-				return;
+        public void MoveFigureLeft()
+        {
+            if (isPause)
+                return;
 
-			await Task.Run(() => MoveFigure(MoveDirection.Left));
-		}
+            Task.Run(() => MoveFigure(MoveDirection.Left));
+        }
 
-		public async Task MoveFigureRight()
-		{
-			if (isPause)
-				return;
+        public void MoveFigureRight()
+        {
+            if (isPause)
+                return;
 
-			await Task.Run(() => MoveFigure(MoveDirection.Right));
-		}
+            Task.Run(() => MoveFigure(MoveDirection.Right));
+        }
 
-		public async Task MoveFigureDown()
-		{
-			if (isPause)
-				return;
+        public void MoveFigureDown()
+        {
+            if (isPause)
+                return;
 
-			await Task.Run(() => MoveFigure(MoveDirection.Down));
-		}
+            Task.Run(() => MoveFigure(MoveDirection.Down));
+        }
 
-		public async Task RotateFigure()
-		{
-			if (isPause)
-				return;
+        public void RotateFigure()
+        {
+            if (isPause)
+                return;
 
-			await Task.Run(() =>
-			{
-				if (!CanRotateFigure())
-					return;
+            Task.Run(() =>
+             {
+                 if (!CanRotateFigure())
+                     return;
 
-				Position[] OldSegmentsPosition = new Position[_figure.SegmentsPosition.Count()];
-				_figure.SegmentsPosition.ToList().CopyTo(OldSegmentsPosition, 0);
+                 Position[] OldSegmentsPosition = new Position[_figure.SegmentsPosition.Count()];
+                 _figure.SegmentsPosition.ToList().CopyTo(OldSegmentsPosition, 0);
 
-				_figure.Rotate();
-				RefillGameField(OldSegmentsPosition);
-				NotifyGameFieldChanged();
-			});
-		}
+                 _figure.Rotate();
+                 RefillGameField(OldSegmentsPosition);
+                 NotifyGameFieldChanged();
+             });
+        }
 
-		private void NotifyGameFieldChanged()
-		{
-			Cell[][] gameFieldCopy = _gameField.Select(x => x.Select(cell => (Cell)cell.Clone()).ToArray()).ToArray();
-			OnGameFieldChanged(gameFieldCopy);
-		}
-		#endregion
+        private void NotifyGameFieldChanged()
+        {
+            Cell[][] gameFieldCopy = _gameField.Select(x => x.Select(cell => (Cell)cell.Clone()).ToArray()).ToArray();
+            OnGameFieldChanged(gameFieldCopy);
+        }
+        #endregion
 
-		#region Game methods
-		private int ProduceGame()
-		{
-			while (TryPutNewFigure())
-			{
-				while (CanMove(MoveDirection.Down))
-				{
-					if (!isPause)
-					{
-						Thread.Sleep(_delay);
-						MoveFigure(MoveDirection.Down);
-					}
-				}
-				var erasedRowsOnThisIter = EraseFilledRows();
-				IncreaseGameSpeed(erasedRowsOnThisIter);
-			}
+        #region Game methods
+        private int ProduceGame()
+        {
+            while (TryPutNewFigure())
+            {
+                while (CanMove(MoveDirection.Down))
+                {
+                    if (!isPause)
+                    {
+                        Thread.Sleep(_delay);
+                        MoveFigure(MoveDirection.Down);
+                    }
+                }
+                var erasedRowsOnThisIter = EraseFilledRows();
+                IncreaseGameSpeed(erasedRowsOnThisIter);
+            }
 
-			isGameOver = true;
-			return _countOfErasedRows;
-		}
+            isGameOver = true;
+            return _countOfErasedRows;
+        }
 
-		private void IncreaseGameSpeed(int incresingCount)
-		{
-			for (int i = 0; i < incresingCount; i++)
-				_delay = Convert.ToInt32(_delay * (1 - DelayDecrisingPercent));
-		}
-		#endregion
+        private void IncreaseGameSpeed(int incresingCount)
+        {
+            for (int i = 0; i < incresingCount; i++)
+                _delay = Convert.ToInt32(_delay * (1 - DelayDecrisingPercent));
+        }
+        #endregion
 
-		#region GameField methods
-		private bool IsSegmentBelongToGameField(Position segment) => (-1 < segment.X && segment.X < Width) && (-1 < segment.Y && segment.Y < Height);
+        #region GameField methods
+        private bool IsSegmentBelongToGameField(Position segment) => (-1 < segment.X && segment.X < Width) && (-1 < segment.Y && segment.Y < Height);
 
-		private void RefillGameField(IEnumerable<Position> figureOldSegmentsPosition)
-		{
-			foreach (var segment in _figure.SegmentsPosition)
-			{
-				_gameField[segment.Y][segment.X].IsFilled = true;
-				_gameField[segment.Y][segment.X].Color = _figure.Color;
-			}
+        private void RefillGameField(IEnumerable<Position> figureOldSegmentsPosition)
+        {
+            foreach (var segment in _figure.SegmentsPosition)
+            {
+                _gameField[segment.Y][segment.X].IsFilled = true;
+                _gameField[segment.Y][segment.X].Color = _figure.Color;
+            }
 
-			foreach (var segment in figureOldSegmentsPosition)
-				if (!_figure.IsBelong(segment))
-				{
-					_gameField[segment.Y][segment.X].IsFilled = false;
-					_gameField[segment.Y][segment.X].Color = Cell.DefaultColor;
-				}
-		}
-		#endregion
+            foreach (var segment in figureOldSegmentsPosition)
+                if (!_figure.IsBelong(segment))
+                {
+                    _gameField[segment.Y][segment.X].IsFilled = false;
+                    _gameField[segment.Y][segment.X].Color = Cell.DefaultColor;
+                }
+        }
+        #endregion
 
-		#region Movement
-		private bool CanRotateFigure()
-		{
-			var displacement = _figure.GetRotationDisplacement();
-			var segmentsPosition = _figure.SegmentsPosition.ToArray();
+        #region Movement
+        private bool CanRotateFigure()
+        {
+            var displacement = _figure.GetRotationDisplacement();
+            var segmentsPosition = _figure.SegmentsPosition.ToArray();
 
-			Position[] newSegmentsPosition = new Position[_figure.SegmentsPosition.Count()];
+            Position[] newSegmentsPosition = new Position[_figure.SegmentsPosition.Count()];
 
-			for (int i = 0; i < segmentsPosition.Length; i++)
-				newSegmentsPosition[i] = segmentsPosition[i] + displacement[i];
+            for (int i = 0; i < segmentsPosition.Length; i++)
+                newSegmentsPosition[i] = segmentsPosition[i] + displacement[i];
 
-			return CanExecuteMovement(newSegmentsPosition);
-		}
+            return CanExecuteMovement(newSegmentsPosition);
+        }
 
-		private bool CanMove(MoveDirection direction)
-		{
-			var moveDirection = GetMoveVector(direction);
-			var segmentsPosition = _figure.SegmentsPosition.ToArray();
+        private bool CanMove(MoveDirection direction)
+        {
+            var moveDirection = GetMoveVector(direction);
+            var segmentsPosition = _figure.SegmentsPosition.ToArray();
 
-			Position[] newSegmentsPosition = new Position[_figure.SegmentsPosition.Count()];
+            Position[] newSegmentsPosition = new Position[_figure.SegmentsPosition.Count()];
 
-			for (int i = 0; i < segmentsPosition.Length; i++)
-				newSegmentsPosition[i] = segmentsPosition[i] + moveDirection;
+            for (int i = 0; i < segmentsPosition.Length; i++)
+                newSegmentsPosition[i] = segmentsPosition[i] + moveDirection;
 
-			return CanExecuteMovement(newSegmentsPosition);
-		}
+            return CanExecuteMovement(newSegmentsPosition);
+        }
 
-		private bool CanExecuteMovement(Position[] newSegmentsPosition)
-		{
-			for (int i = 0; i < newSegmentsPosition.Length; i++)
-			{
-				if (!IsSegmentBelongToGameField(newSegmentsPosition[i]))
-					return false;
+        private bool CanExecuteMovement(Position[] newSegmentsPosition)
+        {
+            for (int i = 0; i < newSegmentsPosition.Length; i++)
+            {
+                if (!IsSegmentBelongToGameField(newSegmentsPosition[i]))
+                    return false;
 
-				var cell = _gameField[newSegmentsPosition[i].Y][newSegmentsPosition[i].X];
-				if (cell.IsFilled && !_figure.IsBelong(newSegmentsPosition[i]))
-					return false;
-			}
+                var cell = _gameField[newSegmentsPosition[i].Y][newSegmentsPosition[i].X];
+                if (cell.IsFilled && !_figure.IsBelong(newSegmentsPosition[i]))
+                    return false;
+            }
 
-			return true;
-		}
+            return true;
+        }
 
-		private void MoveFigure(MoveDirection direction)
-		{
-			if (!CanMove(direction))
-				return;
+        private void MoveFigure(MoveDirection direction)
+        {
+            if (!CanMove(direction))
+                return;
 
-			var moveDirection = GetMoveVector(direction);
-			Position[] OldSegmentsPosition = new Position[_figure.SegmentsPosition.Count()];
-			_figure.SegmentsPosition.ToList().CopyTo(OldSegmentsPosition, 0);
+            var moveDirection = GetMoveVector(direction);
+            Position[] OldSegmentsPosition = new Position[_figure.SegmentsPosition.Count()];
+            _figure.SegmentsPosition.ToList().CopyTo(OldSegmentsPosition, 0);
 
-			_figure.Move(moveDirection);
-			RefillGameField(OldSegmentsPosition.ToArray());
+            _figure.Move(moveDirection);
+            RefillGameField(OldSegmentsPosition.ToArray());
 
-			NotifyGameFieldChanged();
-		}
+            NotifyGameFieldChanged();
+        }
 
-		private Position GetMoveVector(MoveDirection direction)
-		{
-			return direction switch
-			{
-				MoveDirection.Left => new Position(-1, 0),
-				MoveDirection.Right => new Position(1, 0),
-				MoveDirection.Down => new Position(0, 1),
-			};
-		}
-		#endregion
+        private Position GetMoveVector(MoveDirection direction)
+        {
+            return direction switch
+            {
+                MoveDirection.Left => new Position(-1, 0),
+                MoveDirection.Right => new Position(1, 0),
+                MoveDirection.Down => new Position(0, 1),
+            };
+        }
+        #endregion
 
-		#region Creating new figure
-		private bool TryPutNewFigure()
-		{
-			CreateNewFigure();
+        #region Creating new figure
+        private bool TryPutNewFigure()
+        {
+            CreateNewFigure();
 
-			if (!CanPutFigure())
-				return false;
+            if (!CanPutFigure())
+                return false;
 
-			PutFigure();
-			NotifyGameFieldChanged();
+            PutFigure();
+            NotifyGameFieldChanged();
 
-			return true;
-		}
+            return true;
+        }
 
-		private void CreateNewFigure()
-		{
-			_figure = _figureFactory.GetFigure(_startPosition);
-		}
+        private void CreateNewFigure()
+        {
+            _figure = _figureFactory.GetFigure(_startPosition);
+        }
 
-		private void PutFigure()
-		{
-			foreach (var segment in _figure.SegmentsPosition)
-			{
-				_gameField[segment.Y][segment.X].IsFilled = true;
-				_gameField[segment.Y][segment.X].Color = _figure.Color;
-			}
-		}
+        private void PutFigure()
+        {
+            foreach (var segment in _figure.SegmentsPosition)
+            {
+                _gameField[segment.Y][segment.X].IsFilled = true;
+                _gameField[segment.Y][segment.X].Color = _figure.Color;
+            }
+        }
 
-		private bool CanPutFigure()
-		{
-			foreach (var segment in _figure.SegmentsPosition)
-				if (!IsSegmentBelongToGameField(segment) || _gameField[segment.Y][segment.X].IsFilled)
-					return false;
+        private bool CanPutFigure()
+        {
+            foreach (var segment in _figure.SegmentsPosition)
+                if (!IsSegmentBelongToGameField(segment) || _gameField[segment.Y][segment.X].IsFilled)
+                    return false;
 
-			return true;
-		}
-		#endregion
+            return true;
+        }
+        #endregion
 
-		#region Erasing rows
-		private int EraseFilledRows()
-		{
-			int countOfErasedRows = 0;
+        #region Erasing rows
+        private int EraseFilledRows()
+        {
+            int countOfErasedRows = 0;
 
-			for (int i = Height - 1; i > 0;)
-			{
-				if (NeedToDelete(_gameField[i]))
-				{
-					EraseRow(i);
-					_countOfErasedRows++;
-					countOfErasedRows++;
-				}
-				else
-					i--;
-			}
-			return countOfErasedRows;
-		}
+            for (int i = Height - 1; i > 0;)
+            {
+                if (NeedToDelete(_gameField[i]))
+                {
+                    EraseRow(i);
+                    _countOfErasedRows++;
+                    countOfErasedRows++;
+                }
+                else
+                    i--;
+            }
+            return countOfErasedRows;
+        }
 
-		private void EraseRow(int rowNumber)
-		{
-			for (int i = 0; i < Width; i++)
-				_gameField[rowNumber][i].IsFilled = false;
+        private void EraseRow(int rowNumber)
+        {
+            for (int i = 0; i < Width; i++)
+                _gameField[rowNumber][i].IsFilled = false;
 
-			MoveFilledCellsDown(rowNumber);
-		}
+            MoveFilledCellsDown(rowNumber);
+        }
 
-		private void MoveFilledCellsDown(int startRow)
-		{
-			for (int i = 0; i < Width; i++)
-				for (int j = startRow; j > 0; j--)
-					if (_gameField[j - 1][i].IsFilled != false)
-						(_gameField[j][i], _gameField[j - 1][i]) = (_gameField[j - 1][i], _gameField[j][i]);
+        private void MoveFilledCellsDown(int startRow)
+        {
+            for (int i = 0; i < Width; i++)
+                for (int j = startRow; j > 0; j--)
+                    if (_gameField[j - 1][i].IsFilled != false)
+                        (_gameField[j][i], _gameField[j - 1][i]) = (_gameField[j - 1][i], _gameField[j][i]);
 
-		}
+        }
 
-		private bool NeedToDelete(Cell[] row)
-		{
-			for (int i = 0; i < row.Length; i++)
-				if (row[i].IsFilled == false)
-					return false;
+        private bool NeedToDelete(Cell[] row)
+        {
+            for (int i = 0; i < row.Length; i++)
+                if (row[i].IsFilled == false)
+                    return false;
 
-			return true;
-		}
-		#endregion
-	}
+            return true;
+        }
+        #endregion
+    }
 
-	enum MoveDirection
-	{
-		Left, Right, Down
-	}
+    enum MoveDirection
+    {
+        Left, Right, Down
+    }
 }
